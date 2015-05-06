f53f42917d952a9c60a43fd9310b0dc2406807b0
Andrew Ribicki <aribicki@saintjoe.edu>
2015-05-06 14:34:15 -0500

    imported attr.java code for baseline to save time

--- Human.java
+++ Human.java
@@ -1,1 +1,47 @@
+package edu.saintjoe.cs.brianc.attr;
+// These comments are added by Brian Capouch
 
+public class Attr {
+	
+	// Once I create an object and give it a name, it CANNOT be changed!!
+	private final String name;
+	
+	// "value" can be a reference to ANY kind of object!!
+	private Object value = null;
+	
+	/* ---------------------------------------------------------------------- */
+
+	// One-parameter constructor; I know the name, I don't know the value
+	public Attr(String name) {
+		this.name = name;
+	}
+
+	// Two-parameter constructor; I know BOTH the name and the value
+	public Attr(String name, Object value) {
+		this.name = name;
+	    this.value = value;
+	    }
+
+	// Accessor or "getter"
+	public String getname() {
+	    return name;
+	    }
+
+	// Accessor or "getter"
+	public Object getValue() {
+	    return value;
+	    }
+
+	// Setter or mututator for data member "value"
+	public Object setValue(Object newValue) {
+	    Object oldVal = value;
+	    value = newValue;
+	    return oldVal;
+	    }
+
+	// ALL classes should have a "toString" method to visualize their contents
+	//   that usually means just printing their data members
+	public String toString() {
+	    return name + "='" + value + "'";
+	    }
+	}
